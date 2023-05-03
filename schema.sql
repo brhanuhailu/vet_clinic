0071f8b237d348ee007a73ce6745f65f578a376c
brhanuhailu <hailubrhanu29@gmail.com>
2023-05-03 06:59:20 -0700

    schema.sql

--- schema.sql
+++ schema.sql
@@ -12,3 +12,22 @@ weight_kg decimal
 );
 
 ALTER TABLE animals ADD COLUMN species varchar(255);
+
+CREATE TABLE owners (id SERIAL PRIMARY KEY, 
+full_name varchar(255), age integer);
+
+CREATE TABLE species (
+id SERIAL PRIMARY KEY,
+name varchar(255)
+);
+
+Begin
+Alter table animals drop column species
+commit
+
+Alter table animals add column species_id int;
+Alter table animals add constraint FK_species Foreign key (species_id) references species(id);
+
+Alter table animals add column owner_id int;
+Alter table animals add constraint FK_owners Foreign key (owner_id) references owners(id);
+
